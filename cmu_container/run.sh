c1c50df60185e79436a9f8261c6d597777b53ab2
camreed60 <camndon.reed@gmail.com>
2025-06-09 14:03:32 -0400

    ahhhhhjhhhh

--- cmu_container/run.sh
+++ cmu_container/run.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export HOST_UID=$(id -u)
 
 docker compose -f "$SCRIPT_DIR/docker-compose.yml" up -d cmu_container
-docker compose -f "$SCRIPT_DIR/docker-compose.yml" run cmu_container bash
+docker run -it --net=host --ipc=host --rm cmu_exploration:v1
