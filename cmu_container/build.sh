d3b6c8ca7fe019a2702042650fa0dfa88e255384
camreed60 <camndon.reed@gmail.com>
2025-06-09 13:20:16 -0400

    why wont this work

--- cmu_container/build.sh
+++ cmu_container/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export HOST_UID=$(id -u)
 export BUILDKIT_PROGRESS=plain
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
--- cmu_container/run.sh
+++ cmu_container/run.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export HOST_UID=$(id -u)
 
-docker compose -f $SCRIPT_DIR/docker-compose.yml run \
-${REPOSITORY_NAME} bash
+docker compose -f "$SCRIPT_DIR/docker-compose.yml" up -d cmu_container
+docker compose -f "$SCRIPT_DIR/docker-compose.yml" run cmu_container bash
